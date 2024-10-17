92c6b280c309ac47ef9a164c4788cbedd93fe5e5
mikemospan <28594918+mikemospan@users.noreply.github.com>
2024-10-17 16:49:46 +1100

    update

--- projects/aos/sos/src/clock_replacement.h
+++ projects/aos/sos/src/clock_replacement.h
@@ -9,7 +9,7 @@ int clock_page_out();
 
 /**
  * Adds a page in the page table to the clock circular buffer.
- * @param page The page table entry we are adding to our clock cicular buffer
+ * @param vaddr The virtual address adding to our clock cicular buffer
  * @return 0 on success and 1 on failure
  */
 int clock_add_page(seL4_Word vaddr);
